97570efd73c919e849fac85af829fd5c4b1a9de7
cuongtg <cuongtg@unicloud.com.vn>
2024-02-02 22:29:43 +0700

    Fix project structure

---
+++ index.js
@@ -0,0 +1,19 @@
+// !NOT remove this line. call initial configuration
+const initialConf = require('./src/config/firebase-config');
+
+const app = require('./src/middleware/middleware');
+const PORT = process.env.PORT || 3001;
+const profileRouter = require('./src/routes/profileRouter');
+const authRouter = require("./src/routes/authRouter");
+const homeRouter = require("./src/routes/homeRouter");
+const orderRouter = require("./src/routes/orderRouter");
+
+app.use('/', homeRouter);
+app.use('/login', authRouter);
+app.use('/signup', authRouter);
+app.use('/profile', profileRouter);
+app.use('/manage-orders', orderRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
