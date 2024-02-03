07eab6971fa8fdf062a1d5cd52525134586b1c9e
cuongtg <cuongtg@unicloud.com.vn>
2024-02-03 14:38:53 +0700

    Test setver is connected?

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -2,6 +2,11 @@ const express = require('express');
 const cors = require("cors");
 const app = express();
 
+//test setver 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
