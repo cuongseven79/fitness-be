9df7d044ded8ff0f37e5c3b665a42c0106f0144b
cuongtg <cuongtg@unicloud.com.vn>
2024-02-08 23:51:11 +0700

    [ADD] api update rating for Customer role when expired date

--- src/routes/coachRouter.js
+++ src/routes/coachRouter.js
@@ -1,3 +1,4 @@
+const admin = require('firebase-admin');
 const express = require('express');
 const router = express.Router();
 const { User } = require('../config/firebase-config');
@@ -10,12 +11,12 @@ async function handleGetMyCoaches(req, res) {
         // Fetch user data
         const userRef = await User.doc(userId).get();
         const { coachesId, ...restUser } = userRef.data();
-
+        
         // Fetch coaches data
         const coachesData = await Promise.all(coachesId.map(async (coachId) => {
             const coachRef = await User.doc(coachId).get();
-            const { password,rating,price,coachesId,customersId, ...restCoach } = coachRef.data();
-            return restCoach;
+            const { password, price, ...restCoach } = coachRef.data();
+            return {...restCoach, coachId: coachId};
         }));
 
         return res.status(200).json({ message: "get successfully", statusCode:200, coachesData: coachesData });
@@ -24,7 +25,44 @@ async function handleGetMyCoaches(req, res) {
         res.status(500).send("Error fetching coaches");
     }
 }
+async function handleUpdateRating(req, res) {
+    const { newRating, coachId, userId } = req.body;
 
+    // Validate request
+    if (!newRating || typeof newRating !== 'number') return res.status(400).send("Invalid rating");
+    if (!coachId || typeof coachId !== 'string') return res.status(400).send("Invalid coach ID");
+
+    try {
+        const coachRef = User.doc(coachId);
+        const customerRef = User.doc(userId);
+        const coachSnapshot = await coachRef.get();
+
+        if (!coachSnapshot.exists) return res.status(400).send("No document found for coach ID");
+
+        // Add the new rating for coach
+        const ratings = coachSnapshot.data().rating || [];
+        ratings.push(newRating);
+
+        // Update coach's ratings
+        await coachRef.update({ rating: ratings });
+
+        // Remove customerID from customersID list in ROLE COACH
+        const customerId = coachSnapshot.get('customersId').filter(id => id === userId);
+        if (customerId.length > 0) {
+            await coachRef.update({ customersId: admin.firestore.FieldValue.arrayRemove(customerId[0]) });
+        }
+
+        // Remove coachID from coachesID list in ROLE CUSTOMER
+        await customerRef.update({ coachesId: admin.firestore.FieldValue.arrayRemove(coachId) });
+
+        return res.status(200).json({ message: "Update successful", statusCode: 200 });
+    } catch (error) {
+        console.error("Error updating rating:", error);
+        return res.status(500).send("Error updating rating");
+    }
+}
+//For Manage Coaches 
 router.get('/', handleGetMyCoaches);
+router.put('/update', handleUpdateRating);
 module.exports = router;
 
